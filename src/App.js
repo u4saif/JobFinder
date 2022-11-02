c2f746730402f4889fe2c4bb82637f3094dc5242
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-11-02 13:56:30 +0530

    toastify added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Error } from "./pages/Error";
 import { Landing } from "./pages/Landing";
 import { Register } from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path='register' element={<Register />}/>
         <Route path='*' element={<Error />}/>
       </Routes>
+      <ToastContainer position='top-center' />
     </BrowserRouter>
   );
 }
