74c738b154c84b4e28c475660b8e2184b701b230
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-11-03 18:41:29 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Error } from "./pages/Error";
-import { Landing } from "./pages/Landing";
-import { Register } from './pages/Register';
+import { Error, Landing, Register } from '../src/pages/index'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {
-AllJobs,
-AddJob,
-Profile,
-Stats,
-SharedLayout
-} from './pages/dashboard/index';
+import { AllJobs, AddJob, Profile, Stats, SharedLayout } from './pages/dashboard/index';
 
 function App() {
   return (
