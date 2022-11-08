ed9f1c09fab1a55beebbb3c7f6e08040b5fa56f5
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-11-08 16:57:10 +0530

    jobslice created

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserSlice from "./feature/users/UserSlice";
-import JobSlice from "./feature/users/JobSlice";
+import JobSlice from "./feature/jobs/JobSlice";
 export const store=configureStore({
     reducer: {
          user: UserSlice,
